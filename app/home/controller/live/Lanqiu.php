4a92984dc3df1e6ff2fd3c6aa8c31d618a0ed747
wagod <18279697517@163.com>
2023-05-23 15:36:46 +0800

    update

--- app/home/controller/live/Lanqiu.php
+++ app/home/controller/live/Lanqiu.php
@@ -3,10 +3,25 @@
 namespace app\home\controller\live;
 
 use app\home\BaseController;
+use app\home\Tdk;
+use think\App;
+use think\facade\View;
 
 class Lanqiu extends BaseController
 {
+    const RouteTag  = 'live_lanqiu';
+
+    public function __construct(App $app)
+    {
+        parent::__construct($app);
+        $this->getTempPath(self::RouteTag);
+    }
     public function index(){
-        return view();
+
+        //处理tdk
+        $tdk = new Tdk();
+        $this->getTdk(self::RouteTag,$tdk);
+
+        return View::fetch($this->tempPath);
     }
 }
--- app/home/controller/live/Zuqiu.php
+++ app/home/controller/live/Zuqiu.php
@@ -10,25 +10,17 @@ use think\facade\View;
 class Zuqiu extends BaseController
 {
     const RouteTag  = 'live_zuqiu';
-    private $tempPath;
 
     public function __construct(App $app)
     {
         parent::__construct($app);
-        $temp_id = $this->nav[self::RouteTag]['temp_id'];
-        $temp_info = (new \app\commonModel\HomeTempRoute)->getHomeTempRouteById($temp_id);
-        $this->tempPath = $temp_info->temp_path;
+        $this->getTempPath(self::RouteTag);
     }
     public function index(){
-        //获取x天内
-
         //处理tdk
         $tdk = new Tdk();
-        $seo['title'] = $this->replaceTDK($this->nav[self::RouteTag]['web_title'],$tdk);
-        $seo['keywords'] = $this->replaceTDK($this->nav[self::RouteTag]['web_keywords'],$tdk);
-        $seo['description'] = $this->replaceTDK($this->nav[self::RouteTag]['web_desc'],$tdk);
+        $this->getTdk(self::RouteTag,$tdk);
 
-        View::assign('seo',$seo);
         return View::fetch($this->tempPath);
     }
 }
