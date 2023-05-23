8aa4a4ddc6da06ff437068b73af4dc04e797229c
wagod <18279697517@163.com>
2023-05-23 09:55:19 +0800

    update

--- app/home/controller/live/Zuqiu.php
+++ app/home/controller/live/Zuqiu.php
@@ -3,10 +3,32 @@
 namespace app\home\controller\live;
 
 use app\home\BaseController;
+use app\home\Tdk;
+use think\App;
+use think\facade\View;
 
 class Zuqiu extends BaseController
 {
+    const RouteTag  = 'live_zuqiu';
+    private $tempPath;
+
+    public function __construct(App $app)
+    {
+        parent::__construct($app);
+        $temp_id = $this->nav[self::RouteTag]['temp_id'];
+        $temp_info = (new \app\commonModel\HomeTempRoute)->getHomeTempRouteById($temp_id);
+        $this->tempPath = $temp_info->temp_path;
+    }
     public function index(){
-        return view();
+        //获取x天内
+
+        //处理tdk
+        $tdk = new Tdk();
+        $seo['title'] = $this->replaceTDK($this->nav[self::RouteTag]['web_title'],$tdk);
+        $seo['keywords'] = $this->replaceTDK($this->nav[self::RouteTag]['web_keywords'],$tdk);
+        $seo['description'] = $this->replaceTDK($this->nav[self::RouteTag]['web_desc'],$tdk);
+
+        View::assign('seo',$seo);
+        return View::fetch($this->tempPath);
     }
 }
