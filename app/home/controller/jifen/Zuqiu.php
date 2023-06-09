3fc91557f02205cee955c1b2341995904350762a
hb362421@163.com <hb362421@163.com>
2023-06-09 10:31:49 +0800

    updata

--- app/home/controller/jifen/Zuqiu.php
+++ app/home/controller/jifen/Zuqiu.php
@@ -10,30 +10,34 @@ use think\facade\View;
 
 class Zuqiu extends BaseController
 {
-    const RouteTag  = 'jifen_zuqiu';
+    const RouteTag = 'jifen_zuqiu';
+
     public function __construct(App $app)
     {
         parent::__construct($app);
         $this->getTempPath(self::RouteTag);
     }
 
-    public function index(){
+    public function index()
+    {
         $param = $this->parmas;
         $defaultComp = getFootballHotComp(1);
-        $compName = $param['compname']??$defaultComp[0]['short_name_py'];
+        $compName = $param['compname'] ?? $defaultComp[0]['short_name_py'];
 
         //获取联赛
         $comp = FootballCompetition::getByPY($compName);
-        if ($comp->isEmpty()){
+        if ($comp->isEmpty()) {
             abort(404, '参数错误');
         }
 
-        $data = getCompTables(0,0,$comp->id);
+        $data = getCompTables(0, 0, $comp->id);
 
         $this->tdk = new Tdk();
         $this->tdk->short_name_zh = $comp->short_name_zh;
-        $this->getTdk(self::RouteTag,$this->tdk);
-        View::assign('data',$data[0]);
+        $this->getTdk(self::RouteTag, $this->tdk);
+
+        $list = count($data) > 0 ? $data[0] : false;
+        View::assign('data', $list);
         return View::fetch($this->tempPath);
     }
 }
