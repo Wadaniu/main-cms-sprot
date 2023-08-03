036898dbcc54d4c106b76c82eef24545f1137309
xiaowenfu <88598401@qq.com>
2023-08-03 17:30:10 +0800

    本地开发测试

---
+++ app/command/test.php
@@ -0,0 +1,28 @@
+<?php
+declare (strict_types = 1);
+
+namespace app\command;
+
+use think\console\Command;
+use think\console\Input;
+use think\console\input\Argument;
+use think\console\input\Option;
+use think\console\Output;
+
+class test extends Command
+{
+    protected function configure()
+    {
+        // 指令配置
+        $this->setName('test')
+            ->setDescription('the test command');
+    }
+
+    protected function execute(Input $input, Output $output)
+    {
+        $res = getLxAndJjByMatchId(3931546,0);
+        var_dump($res);
+        // 指令输出
+        $output->writeln('test');
+    }
+}
